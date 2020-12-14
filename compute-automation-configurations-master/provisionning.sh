80cdc49549ce17e2031b9fb76ff819f1ff47b77e
junheefr789 <junheefr789@gmail.com>
2020-12-14 17:11:28 +0900

    easff

--- compute-automation-configurations-master/provisionning.sh
+++ compute-automation-configurations-master/provisionning.sh
@@ -4,5 +4,4 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y python3-pip
 sudo python3 -m pip install azure-servicebus
-sleep 30s
-sudo python3 /var/lib/waagent/custom-script/download/1/test.py
+sudo python3 /var/lib/waagent/custom-script/download/1/test2.py
